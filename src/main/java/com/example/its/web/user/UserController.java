e9d41f8f7e3b795a79eeacc4d3728226dbee35c4
uijong0729 <krocha0729@gmail.com>
2023-01-14 22:30:07 +0900

    컨트롤러 + 액션

--- src/main/java/com/example/its/web/user/UserController.java
+++ src/main/java/com/example/its/web/user/UserController.java
@@ -6,6 +6,7 @@ import java.util.Collections;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.its.domain.user.UserService;
@@ -29,6 +30,13 @@ public class UserController {
     @GetMapping("/creationForm")
     public String showCreateForm() {
         return "users/creationForm";
+    }
+
+    // Post /users
+    @PostMapping
+    public String create() {
+
+        return "redirect:/users";
     }
     
 }
