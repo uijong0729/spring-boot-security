503ba958e299527e0054f51f39f7ab857e0f5de3
uijong0729 <krocha0729@gmail.com>
2023-01-19 20:53:22 +0900

    model 받기

--- src/main/java/com/example/its/web/user/UserController.java
+++ src/main/java/com/example/its/web/user/UserController.java
@@ -6,13 +6,16 @@ import java.util.Collections;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.its.domain.user.UserService;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 
+@Log4j2
 @Controller
 @RequestMapping("/users")
 @AllArgsConstructor
@@ -20,6 +23,7 @@ public class UserController {
     
     private final UserService userService;
 
+    // Get /users
     @GetMapping
     public String showList(Model model) {
         model.addAttribute("userList", userService.findAll());
@@ -28,15 +32,17 @@ public class UserController {
     }
 
     @GetMapping("/creationForm")
-    public String showCreateForm() {
+    public String showCreateForm(@ModelAttribute UserForm form) {
+        log.info("showCreateForm : " + form.getUsername());
         return "users/creationForm";
     }
 
     // Post /users
     @PostMapping
-    public String create() {
-
+    public String create(UserForm form) {
+        log.info("create : " + form.getUsername());
         return "redirect:/users";
     }
+
     
 }
