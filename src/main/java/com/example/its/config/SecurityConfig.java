4b76d2a416a43809a3ce3052fe67c1b330574a06
krocha0729@gmail.com <krocha0729@gmail.com>
2023-02-13 23:41:15 +0900

    로그인 안되던 현상 수정

--- src/main/java/com/example/its/config/SecurityConfig.java
+++ src/main/java/com/example/its/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.example.its.domain.auth.CustomUserDetailService;
@@ -51,6 +52,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth
             .userDetailsService(userDetailsService)
             //passowrd encoder : 패스워드를 해쉬화 하기 위함
-            .passwordEncoder(passwordEncoder);
+            .passwordEncoder(passwordEncoder)
+            // .passwordEncoder(NoOpPasswordEncoder.getInstance())
+            ;
     }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,5 +2,6 @@
 insert into issues (summary, description) values ('バグA', 'バグがあります');
 insert into issues (summary, description) values ('機能要望B', 'Bに追加機能がほしいです');
 insert into issues (summary, description) values ('画面Cが遅い', '早くしてほしいです');
-insert into users (username, password) values ('username', 'password');
-insert into users (username, password) values ('user', 'password');
+
+-- pw : 123412341234
+insert into users (username, password, authority) values ('myuser', 'b5e27f42ec1693db70cd788b8e39921a553d3aecff89b624196ccab2790598725e9b4b90feac479e', 'USER');
