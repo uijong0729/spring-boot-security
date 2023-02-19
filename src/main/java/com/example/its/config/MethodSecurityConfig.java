d4b9301991b1cc658c0c7520fa649f010eceeb4f
krocha0729@gmail.com <krocha0729@gmail.com>
2023-02-19 17:33:12 +0900

    권한체크를 메소드에도 지정

---
+++ src/main/java/com/example/its/config/MethodSecurityConfig.java
@@ -0,0 +1,8 @@
+package com.example.its.config;
+
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class MethodSecurityConfig {
+    
+}
--- src/main/java/com/example/its/domain/auth/UserService.java
+++ src/main/java/com/example/its/domain/auth/UserService.java
@@ -2,11 +2,11 @@ package com.example.its.domain.auth;
 
 import java.util.List;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
 @Service
 @AllArgsConstructor
@@ -15,10 +15,13 @@ public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
+    // 메소드별로 권한 체크를 부여할 수 있다.
+    @PreAuthorize("hasAuthority('ADMIN')")
     public List<User> findAll() {
         return userRepository.findAll();
     }
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     public void create(String username, String password) {
         // 인코드
         var encodedPassword = passwordEncoder.encode(password);
